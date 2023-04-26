bfb900f438098e589a5a2928a1dd7057253b0c98
MiqeG <mghilescu@protonmail.com>
2023-04-26 19:46:49 +0200

    done adding loader

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,12 +1,26 @@
 import { Outlet } from "react-router-dom";
 import Menu from "../components/Menu";
+import React, { useEffect } from "react";
+import loader from "../audio.svg";
 
 const Layout = () => {
-  return (
+  const [isLoading, setIsLoading] = React.useState(true);
+  useEffect(() => {
+    window.addEventListener("load", handleLoading);
+    return () => window.removeEventListener("load", handleLoading);
+  }, []);
+  const handleLoading = () => {
+    setIsLoading(false);
+  };
+  return !isLoading ? (
     <>
       <Menu />
       <Outlet />
     </>
+  ) : (
+    <div className="loader_image">
+      {<img class="loader_image" src={loader} alt="loader" />}
+    </div>
   );
 };
 
